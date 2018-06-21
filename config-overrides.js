2d1791eed3ce8eed1fb6b17b694b590d13dc6586
Ham <pallop.chao@gmail.com>
2018-06-21 14:31:48 +0700

    ✏️ Draft

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+const { injectBabelPlugin } = require('react-app-rewired')
+
+module.exports = function override(config, env) {
+  config = injectBabelPlugin('emotion', config)
+
+  return config
+}
