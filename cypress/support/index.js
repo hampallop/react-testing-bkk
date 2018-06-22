ddc487b768d7e57dfc186957bc06bee59a5b27fe
Ham <pallop.chao@gmail.com>
2018-06-23 03:55:14 +0700

    ✨ Add cypress-testing-library

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import 'cypress-testing-library/add-commands'
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
